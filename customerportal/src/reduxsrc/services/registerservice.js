82ed18051ef06eaeb67425d932f78e0b9f94bb8e
Parameswari Bala <parameswaribala@gmail.com>
2024-03-21 12:52:11 +0530

    updated

--- customerportal/src/reduxsrc/services/registerservice.js
+++ customerportal/src/reduxsrc/services/registerservice.js
@@ -1,6 +1,8 @@
-import {create} from "axios";
+import * as http from "http";
 
-
+const create = (data) => {
+    return http.post("/api/v1/Customers", data);
+};
 const RegisterService={
     create
 }
