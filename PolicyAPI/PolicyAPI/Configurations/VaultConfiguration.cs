736a302d18e0511796899cdfb01a0c33e204734e
Parameswari Bala <parameswaribala@gmail.com>
2024-03-28 12:53:17 +0530

    updated

---
+++ PolicyAPI/PolicyAPI/Configurations/VaultConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using VaultSharp;
+using VaultSharp.V1.AuthMethods.Token;
+
+namespace PolicyAPI.Configurations
+{
+    public class VaultConfiguration
+    {
+        private IConfiguration _configuration;
+
+        public VaultConfiguration(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        public async Task<Dictionary<string,object>>  GetSecrets()
+        {
+            var url = _configuration["Vault_Url"];
+            var rootToken = _configuration["Root_Token"];
+
+            TokenAuthMethodInfo tokenAuthMethodInfo=
+                new TokenAuthMethodInfo(rootToken);
+
+           VaultClientSettings  vaultClientSettings
+                =new VaultClientSettings(url, tokenAuthMethodInfo);
+            IVaultClient vaultClient = new VaultClient(vaultClientSettings);
+            Console.WriteLine(vaultClient.V1.Secrets);
+
+            var result = await vaultClient.V1.Secrets.KeyValue.V1.ReadSecretAsync("sqlserver2019",
+                "secret", null);
+            return result.Data;
+
+
+        }
+    }
+}
