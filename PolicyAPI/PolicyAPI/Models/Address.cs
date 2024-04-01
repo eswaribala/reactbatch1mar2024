061b73565c5f0900cbce4beeca62949e92ae8686
Parameswari Bala <parameswaribala@gmail.com>
2024-04-01 10:21:17 +0530

    updated

---
+++ PolicyAPI/PolicyAPI/Models/Address.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PolicyAPI.Models
+{
+    [Table("Address")]
+    public class Address
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("Address_Id")]
+        public long AddressId {  get; set; }
+        [Column("Door_No")]
+        public string DoorNo { get; set; }
+        [Column("Street_Name")]
+        public string StreetName {  get; set; }
+        [Column("City")]
+        public string City { get; set; }
+        [Column("State")]
+        public string State { get; set; }
+        [Column("Country")]
+        public string Country { get; set; }
+        [Column("Postal_Code")]
+        public long PostalCode { get; set; }
+
+
+        [ForeignKey("PolicyHolder")]
+        [Column("AdharCard_No_FK")]
+        public string AdharCardNo {  get; set; }
+
+        public PolicyHolder PolicyHolder { get; set; }
+
+
+
+    }
+}
---
+++ PolicyAPI/PolicyAPI/Models/Vehicle.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PolicyAPI.Models
+{
+    public enum FuelType
+    {
+        PETROL,DIESEL,ELECTRIC,GAS
+    }
+
+    [Table("Vehicle")]
+    public class Vehicle
+    {
+        [Key]
+        [Column("Registration_No")]
+        public string RegistrationNo {  get; set; }
+        [Column("Maker")]
+        public string Maker {  get; set; }
+        [Column("DOR")]
+        [DataType(DataType.Date)]
+        public DateTime DOR {  get; set; }
+        [Column("Engine_No")]
+        public string EngineNo {  get; set; }
+        [Column("Chasis_No")]
+        public string ChasisNo {  get; set; }
+        [Column("Color")]
+        public string Color {  get; set; }
+        [Column("Fuel_Type")]
+        public FuelType FuelType { get; set;}
+
+    }
+}
